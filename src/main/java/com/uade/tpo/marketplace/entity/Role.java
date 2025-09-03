ccd856d73f187ed91c8f6c07b7b263d0419d6c54
JuanPeiretti <juan.b_77@hotmail.com>
2025-09-03 19:49:58 -0300

    ver nueva

--- src/main/java/com/uade/tpo/marketplace/entity/Role.java
+++ src/main/java/com/uade/tpo/marketplace/entity/Role.java
@@ -1,27 +1,16 @@
 package com.uade.tpo.marketplace.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import lombok.Data;
 
-import java.util.List;
+import jakarta.persistence.Entity;
+
+
+
 
 
 @Entity
-@Data
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column
-    private String description;
-
-    @ManyToMany(mappedBy = "roles")
-    private List<User> users;
+public enum Role {
+    USER,
+    ADMIN
 }
+
 
---
+++ src/main/java/com/uade/tpo/marketplace/entity/userDetails.java
@@ -0,0 +1,5 @@
+package com.uade.tpo.marketplace.entity;
+
+public interface userDetails {
+
+}
