80178fce9f2001200fb7eea4405247d6ab64564a
JuanPeiretti <63617490+JuanPeiretti@users.noreply.github.com>
2025-09-05 19:20:37 -0300

    Update GameController.java

--- src/main/java/com/uade/tpo/marketplace/controller/Game/GameController.java
+++ src/main/java/com/uade/tpo/marketplace/controller/Game/GameController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.uade.tpo.marketplace.entity.Game;
 import com.uade.tpo.marketplace.service.GameService;
@@ -23,6 +25,8 @@ import com.uade.tpo.marketplace.service.GameService;
  *
  * @author juanb
  */
+@RestController
+@RequestMapping("games")
 public class GameController {
     @Autowired
     private GameService GameService;
