e85e900bfa3ab7c4b144303ee2c679a30c49da1b
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-10-18 14:56:04 -0300

    Update OrderService.java

--- src/main/java/com/uade/tpo/marketplace/service/OrderService.java
+++ src/main/java/com/uade/tpo/marketplace/service/OrderService.java
@@ -4,7 +4,13 @@ import com.uade.tpo.marketplace.controller.order.OrderRequest;
 import com.uade.tpo.marketplace.controller.order.OrderResponse;
 import com.uade.tpo.marketplace.entity.User;
 import com.uade.tpo.marketplace.exceptions.NoStockAvailableException;
+import java.util.List;
 
 public interface OrderService {
+
+  
     public OrderResponse createOrder(User user, OrderRequest orderRequest) throws NoStockAvailableException;
+
+    public List<OrderResponse> getOrdersByUserEmail(String email);
+
 }
