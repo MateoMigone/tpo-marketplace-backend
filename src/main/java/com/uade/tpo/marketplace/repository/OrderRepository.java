f48faa861b64e0273ee854088b7f0c31d643ef01
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-10-18 15:16:04 -0300

    Update OrderRepository.java

--- src/main/java/com/uade/tpo/marketplace/repository/OrderRepository.java
+++ src/main/java/com/uade/tpo/marketplace/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.uade.tpo.marketplace.repository;
 
 import com.uade.tpo.marketplace.entity.Order;
+import com.uade.tpo.marketplace.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+  List<Order> findByUser(User user);
 }
