b67ddf160c3c8d9b3141df8b3eb3a48720a0939a
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-11-22 20:02:26 -0300

    Update OrderService.java

--- src/main/java/com/uade/tpo/marketplace/service/OrderService.java
+++ src/main/java/com/uade/tpo/marketplace/service/OrderService.java
@@ -5,7 +5,12 @@ import com.uade.tpo.marketplace.controller.order.OrderResponse;
 import com.uade.tpo.marketplace.entity.User;
 import com.uade.tpo.marketplace.exceptions.NoStockAvailableException;
 
+import java.util.List; // Asegúrate de importar List
+
 public interface OrderService {
     public OrderResponse createOrder(User user, OrderRequest orderRequest) throws NoStockAvailableException;
-    public java.util.List<OrderResponse> getOrdersForUser(User user);
+    public List<OrderResponse> getOrdersForUser(User user);
+    
+    // NUEVO MÉTODO PARA EL ADMINISTRADOR
+    public List<com.uade.tpo.marketplace.entity.Order> findAllOrders();
 }
